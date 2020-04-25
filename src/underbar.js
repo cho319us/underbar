813f062816494ed9742e332aae77608e7e39992a
cho319 <cho319us@gmail.com>
2020-04-24 20:52:10 -0700

    Passed all tests in filter and reject

--- src/underbar.js
+++ src/underbar.js
@@ -88,12 +88,22 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var filterArr = [];
+    _.each(collection, function(element) {
+      if (test(element)) {
+        filterArr.push(element);
+      }
+    });
+    return filterArr;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(element){
+      return !test(element);
+    });
   };
 
   // Produce a duplicate-free version of the array.
